40dbd666122f4de552c6c08e266c01066d8dba8b
Sam Norbury <sam.norbury@ukfast.co.uk>
2016-09-27 11:57:29 +0100

    Now with correct path

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /var/www/vhosts/docs.ukfast.co.uk/docs.ukfast.co.uk
 git pull origin master
-cd sekhmet && git pull origin master && cd -
+cd ukf && git pull origin master && cd -
 curl -XDELETE 'http://localhost:9200/documentation/'
 make populate-index
