778d7407c2c05738fa3613226d8f732d7dc9a158
Sam Norbury <sam.norbury@ukfast.co.uk>
2016-09-27 11:54:28 +0100

    ES building script

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-cd /var/www/vhosts/docs.ukfast.co.uk
+cd /var/www/vhosts/docs.ukfast.co.uk/docs.ukfast.co.uk
 git pull origin master
 cd sekhmet && git pull origin master && cd -
-make clean && make html
 curl -XDELETE 'http://localhost:9200/documentation/'
 make populate-index
